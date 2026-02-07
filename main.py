0c2ba7d33bf97e324bb25bf1dccfa547f31c21c2
Gustavo-Herculano <gustavoherculanoqa@gmail.com>
2026-02-07 17:54:22 -0300

    /user/details/{id_user} - implementando o segundo endpoint

--- main.py
+++ main.py
@@ -30,3 +30,24 @@ def user_list():
     conexao.close()
     return {"users": resultados}
 
+@app.get("/user/details/{id_user}")
+def user_details(id_user: int):
+    # conexao com o banco de dado
+    conexao = mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='micklindo',
+        database='poc_api_php'
+    )
+    cursor = conexao.cursor(dictionary=True)
+    # Consulta segura usando parâmetros
+    cursor.execute("SELECT * FROM poc_api_php.users WHERE id = %s", (id_user,))
+    resultado = cursor.fetchone()
+    # Fecha a conexao com o bamco de dados
+    cursor.close()
+    conexao.close()
+    if resultado:
+        return resultado
+    else:
+        return {"error": "Usuário não encontrado"}, 404
+
